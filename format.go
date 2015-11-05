ca258abc75da2a35caff0adbfb5e40d68880c042
joshua bowles <bowlesling@gmail.com>
2015-11-05 12:20:55 -0700

    use a struct for money formatting

--- format.go
+++ format.go
@@ -5,30 +5,24 @@ import (
 	"golang.org/x/text/currency"
 )
 
-func (m *Money) Format(isoCode string) (string, string, *Money) {
-	code, _ := currency.ParseISO(isoCode)
-	var symbol = currency.NarrowSymbol
-	// symbol(code) is a currency.Value and has a Format() function but it seems easier to use fmt.
-	symString := fmt.Sprintf("%v", symbol(code))
-	return symString, code.String(), m
+type MoneyFormat struct {
+	MoneyVal *Money
+	IsoCode  string
+	Symbol   string
+	Display  string
 }
 
-/*
-AN EXAMPLE OF UISNG SOME OF THE FORMATTING FROM text/currency. CURRENTLY INTEGERS NOT SUPPORTED, AND NOT COMMA VERSUS DECIMAL DELIMITER IN STRING METHODS
-  	m := money.Money{123456}
-	FormatFloat64(m.Valuef(), "DKK")
-func FormatFloat64(m float64, isoCode string) {
-	c, _ := currency.ParseISO(isoCode)
-	var nsymbol = currency.NarrowSymbol
-	var symbol = currency.Symbol
-	var iso = currency.ISO
-	var symbKindCash = currency.Symbol.Kind(currency.Cash)
-	fmt.Printf("c.String() with c: %v, c.Value(m) with c: %v\n", c.String(), c.Value(m))
+var currencySymbolFunc = currency.NarrowSymbol
 
-	val := c.Value(m)
-	fmt.Printf("narrow symbol formatter with v: %v\n", nsymbol(val))
-	fmt.Printf("symbol formatter with v: %v\n", symbol(val))
-	fmt.Printf("iso formatter with v: %v\n", iso(val))
-	fmt.Printf("symbol kind cash formatter with value: %v\n", symbKindCash(val))
+// Format returns a money format struct with the money int64 value, as well as curency code and symbol
+// NOTE that `symbol(code)` is a `currency.Value` and has a `Format()` function but it seems easier to use fmt.
+func (m *Money) Format(isoCode string) (MoneyFormat, error) {
+	var mf = MoneyFormat{MoneyVal: m}
+	code, err := currency.ParseISO(isoCode)
+	if err != nil {
+		return mf, err
+	}
+	mf.IsoCode = code.String()
+	mf.Symbol = fmt.Sprintf("%v", currencySymbolFunc(code))
+	return mf, nil
 }
-*/
