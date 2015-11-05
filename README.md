28254e1a413b710e9fc280700f70d42abf52b9c1
joshua bowles <bowlesling@gmail.com>
2015-11-04 23:19:56 -0700

    use i18 formatting for money string display

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 This is a rip-off of an old package called [finance](https://github.com/Confunctionist/finance) by `Confunctionist` on github. Credit is given in the `external` directory along with a copy of the original license.
 
 Uses as its common value type `int64` but supports various operations for `float64`.
+
+
+## Benchmarks
+
+```sh
+go test -bench Format
+
+BenchmarkFormatUSD-8   	 1000000	      1944 ns/op
+BenchmarkFormatI18USD-8	 1000000	      2314 ns/op
+```
--- format.go
+++ format.go
@@ -2,33 +2,51 @@ package money
 
 import (
 	"fmt"
+	"github.com/jbowles/i18n"
 	"golang.org/x/text/currency"
 )
 
-func (m *Money) Format(isoCode string) (string, string, *Money) {
-	code, _ := currency.ParseISO(isoCode)
-	var symbol = currency.NarrowSymbol
+var FormatTranslator, _ = i18n.NewTranslatorFactory(
+	[]string{"i18_data/rules"},
+	[]string{"i18_data/messages"},
+	"en",
+)
+
+type MoneyFormat struct {
+	MoneyVal *Money
+	IsoCode  string
+	Symbol   string
+	Display  string
+}
+
+var currencySymbolFunc = currency.NarrowSymbol
+var symbKindCash = currency.Symbol.Kind(currency.Cash)
+
+func (m *Money) Format(isoCode string) (MoneyFormat, error) {
+	var mf = MoneyFormat{MoneyVal: m}
+	code, err := currency.ParseISO(isoCode)
+	if err != nil {
+		return mf, err
+	}
+	mf.IsoCode = code.String()
 	// symbol(code) is a currency.Value and has a Format() function but it seems easier to use fmt.
-	symString := fmt.Sprintf("%v", symbol(code))
-	return symString, code.String(), m
+	mf.Symbol = fmt.Sprintf("%v", currencySymbolFunc(code))
+	return mf, nil
 }
 
-/*
-AN EXAMPLE OF UISNG SOME OF THE FORMATTING FROM text/currency. CURRENTLY INTEGERS NOT SUPPORTED, AND NOT COMMA VERSUS DECIMAL DELIMITER IN STRING METHODS
-  	m := money.Money{123456}
-	FormatFloat64(m.Valuef(), "DKK")
-func FormatFloat64(m float64, isoCode string) {
-	c, _ := currency.ParseISO(isoCode)
-	var nsymbol = currency.NarrowSymbol
-	var symbol = currency.Symbol
-	var iso = currency.ISO
-	var symbKindCash = currency.Symbol.Kind(currency.Cash)
-	fmt.Printf("c.String() with c: %v, c.Value(m) with c: %v\n", c.String(), c.Value(m))
+func (m *Money) Formati18Display(isoCode, language string) (MoneyFormat, error) {
+	mf, err := m.Format(isoCode)
+	if err != nil {
+		return mf, err
+	}
 
-	val := c.Value(m)
-	fmt.Printf("narrow symbol formatter with v: %v\n", nsymbol(val))
-	fmt.Printf("symbol formatter with v: %v\n", symbol(val))
-	fmt.Printf("iso formatter with v: %v\n", iso(val))
-	fmt.Printf("symbol kind cash formatter with value: %v\n", symbKindCash(val))
+	tlang, _ := FormatTranslator.GetTranslator(language)
+	d, err := tlang.FormatCurrency(mf.MoneyVal.Valuef(), isoCode)
+	if err != nil {
+		return mf, err
+	}
+
+	mf.Display = (d + " " + mf.IsoCode)
+
+	return mf, nil
 }
-*/
